5bc20d9356f7abc7c361c3da170217598e8a2817
Boyan Vushkov <bvushkov@gmail.com>
2017-10-13 00:08:23 +0300

    Put a more meaningful test for the second exercise

--- exercises/01.introduction/02-sum-squares-bigger-two.rkt
+++ exercises/01.introduction/02-sum-squares-bigger-two.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require "helpers.rkt")
 (require rackunit)
 (require rackunit/text-ui)
 ;1.3 - Съчинете процедура, която по дадени три числа, намира сумата от квадратите на по-големите две от тях.
@@ -12,14 +13,12 @@
   (test-suite
     "Sum of squares tests"
 
-    (test-case "Random tests"
-      (check-equal?
-        (squares-sum-bigger-two 1 5 6)
-        61
-      )
-      (check-equal?
-        (squares-sum-bigger-two 2 3 3)
-        18
-      ))))
+    (let ((a (random 10))
+          (b (random 10))
+          (c (random 10)))
+      
+    (check-true (all-equal? (map (lambda (args) (apply squares-sum-bigger-two args))
+                                (permute (list a b c)))))
+)))
 
 (run-tests tests 'verbose)
