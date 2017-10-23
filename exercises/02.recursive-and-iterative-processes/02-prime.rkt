621abb20f85a16298dc3ec7fa316b4f80b5d0097
Alexander <doykov70@gmail.com>
2017-10-23 23:23:46 +0300

    week 1 and week 2 solutions

--- exercises/02.recursive-and-iterative-processes/02-prime.rkt
+++ exercises/02.recursive-and-iterative-processes/02-prime.rkt
@@ -1,11 +1,18 @@
 #lang racket
 (require rackunit)
 (require rackunit/text-ui)
-
 ; Искаме да проверим дали число е просто.
 
 (define (prime? number)
-  (void)
+  (define (prime-helper number num)
+    (cond
+      ((or (= number 0) (= number 1)) #f)
+      ((= (modulo number num) 0) #f)
+      ((<= num (sqrt number)) #t)
+      (else (prime-helper number (- num 1)))
+      )
+    )
+   (prime-helper number (- number 1))
 )
 
 (define tests
--- exercises/02.recursive-and-iterative-processes/07-ends-with.rkt
+++ exercises/02.recursive-and-iterative-processes/07-ends-with.rkt
@@ -5,7 +5,11 @@
 ; Търсим процедура, която проверява дали дадено число завършва на дадено друго.
 
 (define (ends-with? number test)
-  (void)
+  (cond
+    ((< test 10) (if (eq? (modulo number 10) (modulo test 10)) #t #f))
+    ((not (eq? (modulo number 10) (modulo test 10))) #f)
+    (else (ends-with? (quotient number 10) (quotient test 10)))
+  )
 )
 
 (define tests
@@ -13,9 +17,9 @@
     (check-true (ends-with? 8317 17))
     (check-true (ends-with? 82 82))
     (check-false (ends-with? 8213 31))
+    (check-false (ends-with? 211 0))
     (check-true (ends-with? 210 0))
     (check-false (ends-with? 2921 2))
-    (check-false (ends-with? 213 0))
   )
 )
 
