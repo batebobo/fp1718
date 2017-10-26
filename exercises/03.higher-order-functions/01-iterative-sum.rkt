38c28c0b46f5f84afe01571a26f347d16b2c5fbe
Alexander <doykov70@gmail.com>
2017-10-26 17:19:57 +0300

    some solutions for week 2

--- exercises/03.higher-order-functions/01-iterative-sum.rkt
+++ exercises/03.higher-order-functions/01-iterative-sum.rkt
@@ -6,10 +6,17 @@
 ; Да стане по итеративен начин.
 
 (define (sum start end term next)
-  (void)
+  (define (helper start end term next result)
+    (if(> start  end)
+       result
+       (helper (next start) end term next (+ result (term start)))
+       )
+    )
+  (helper start end term next 0)
 )
 
 (define (id x) x)
+(define (square x) (* x x))
 (define (inc x) (+ x 1))
 
 (define tests
@@ -17,6 +24,7 @@
 
     (check-equal? (sum 1 100 id inc) 5050)
     (check-equal? (sum 9 9 id inc) 9)
+    (check-equal? (sum 1 10 square inc) 385)
   )
 )
 
