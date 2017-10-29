3bd13f9923d0797fcdca09d553dbd912ebc6fc9a
Boyan Vushkov <bvushkov@gmail.com>
2017-10-29 18:03:10 +0200

    replace idiotic test with a less idiotic one

--- exercises/03.higher-order-functions/05-all-in-interval.rkt
+++ exercises/03.higher-order-functions/05-all-in-interval.rkt
@@ -13,7 +13,7 @@
   (test-suite "All interval tests"
     (check-true (all-interval? 2 9 (lambda (x) (+ x 2)) (lambda (x) (< x 10))))
     (check-true (all-interval? 3 13 (lambda (x) (+ x 2)) odd?))
-    (check-false (all-interval? 3 14 (lambda (x) (+ x 2)) odd?))
+    (check-false (all-interval? 3 11 (lambda (x) (+ x 2)) (lambda (x) (not (= (remainder x 3) 1)))))
   )
 )
 
