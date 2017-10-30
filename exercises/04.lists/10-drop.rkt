950022a80c2a23c1249f16fff8fe8b084f2769a6
Alexander <doykov70@gmail.com>
2017-10-30 23:17:40 +0200

    solutions for week 4

--- exercises/04.lists/10-drop.rkt
+++ exercises/04.lists/10-drop.rkt
@@ -4,9 +4,54 @@
 
 ; Търсим функция, която връща списък от всички без първите n елемента на даден такъв.
 
-(define (drop n xs)
-  (void)
+(define (append xs ys)
+  (if(null? xs)
+     ys
+     (cons (car xs) (append (cdr xs) ys)))
 )
+
+(define (reverse xs)
+  (if(null? xs)
+     '()
+     (append (reverse (cdr xs)) (list (car xs)))
+     )
+)
+
+(define (length xs)
+  (define (helper xs size)
+    (if(null? xs)
+       size
+       (helper (cdr xs) (+ size 1))
+       )
+    )
+  (helper xs 0)
+)
+
+;тук и двете функции drop работят просто за втория вариант се сетих в последствие решавайки последната задачая
+
+;(define (drop n xs)
+;  (define (helper n xs-reverse)
+;    (if(< n 0)
+;       '()
+;       (if(or (= n 0)
+;              (null? xs-reverse))
+;          '()
+;          (append  (helper (- n 1) (cdr xs-reverse)) ( list(car xs-reverse)))
+;          )
+;    )
+;    )
+;    (helper (- (length xs) n) (reverse xs))
+;)
+
+(define (drop n xs)
+  (if(null? xs)
+     '()
+  (if(= n 0)
+     (cons (car xs) (drop n (cdr xs)))
+     (drop (- n 1) (cdr xs))
+     )
+  )
+  )
 
 (define tests
   (test-suite "Take tests"
--- exercises/04.lists/11-slice.rkt
+++ exercises/04.lists/11-slice.rkt
@@ -6,7 +6,20 @@
 ; списък, състоящ се от елементите на списъка, които се намират на индекси от първото число до второто.
 
 (define (slice xs start end)
-  (void)
+  (define (helper xs start end counter)
+    (if(null? xs)
+       '()
+       (if(and (>= counter start)
+               (<= counter end))
+          (cons (car xs) (helper (cdr xs) start end (+ counter 1)))
+          (if(< counter start)
+             (helper (cdr xs) start end (+ counter 1))
+             '()
+             )
+          )
+    )
+  )
+  (helper xs start end 0)
 )
 
 (define tests
