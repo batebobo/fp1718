b7511cb4ef4af86c651578386d00ea9498d46a35
Boyan Vushkov <bvushkov@gmail.com>
2017-11-02 22:42:44 +0200

    Remove redundant implementation

--- exercises/04.lists/solutions/05-list-ref.rkt
+++ exercises/04.lists/solutions/05-list-ref.rkt
@@ -4,21 +4,6 @@
 
 ; Искаме да вземем i-тия елемент от списъка lst, като броим от 0.
 
-; Итеративен вариант
-(define (list-ref lst i)
-  (define (helper lst i curr)
-    (if (null? lst)
-       #f
-       (if (= curr i)
-          (car lst)
-          (helper (cdr lst) i (+ curr 1))
-       )
-    )
-  )
-  (helper lst i 0)
-)
-
-; Рекурсивен вариант
 (define (list-ref-rec lst i)
   (cond ((null? lst) (error "Empty list"))
         ((= i 0) (car lst))
