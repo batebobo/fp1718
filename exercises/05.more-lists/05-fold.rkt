e39137d94e279949b9077a81a0311d80ae49a018
Alexander <doykov70@gmail.com>
2017-11-06 17:30:42 +0200

    small fix week 5

--- exercises/05.more-lists/05-fold.rkt
+++ exercises/05.more-lists/05-fold.rkt
@@ -5,17 +5,11 @@
 ; Като accumulate, но със списък, вместо с интервал.
 
 (define (fold op null-value xs)
-  (if(null? xs)
-     null-value
-     (op (car xs) (fold op null-value (cdr xs))))
+  (void)
 )
 
 (define tests
   (test-suite "Fold tests"
-     (check-equal? (fold + 0 '(1 2 3 4 5)) 15)
-     (check-equal? (fold * 1 '(1 2 3 )) 6)
+     ; ???
   )
 )
-
-
-(run-tests tests 'verbose)
--- exercises/05.more-lists/Solutions/05-fold.rkt
+++ exercises/05.more-lists/Solutions/05-fold.rkt
@@ -5,11 +5,17 @@
 ; Като accumulate, но със списък, вместо с интервал.
 
 (define (fold op null-value xs)
-  (void)
+  (if(null? xs)
+     null-value
+     (op (car xs) (fold op null-value (cdr xs))))
 )
 
 (define tests
   (test-suite "Fold tests"
-     ; ???
+     (check-equal? (fold + 0 '(1 2 3 4 5)) 15)
+     (check-equal? (fold * 1 '(1 2 3 )) 6)
   )
 )
+
+
+(run-tests tests 'verbose)
