b0d6ee42080a91e85fe107da51ebd6301cf0d071
Boyan Vushkov <bvushkov@gmail.com>
2017-11-13 20:29:37 +0200

    Fix transpose tests

--- homework/racket/07-transpose.rkt
+++ homework/racket/07-transpose.rkt
@@ -4,14 +4,14 @@
 (require "02-make-matrix.rkt")
 
 ; Търсим транспонираната матрица на дадена такава
+
 (define (transpose m)
-  (void)
-)
+  (void))
 
 (define tests
   (test-suite "Transpose tests"
-    (check-equal? (transpose (make-matrix (range 1 7) 2 3)) (make-matrix (range 1 7) 3 2))
-    (check-equal? (transpose (make-matrix (range 7 13) 3 2)) (make-matrix (range 7 13) 2 3))
+    (check-equal? (transpose (make-matrix (range 1 7) 2 3)) (make-matrix '(1 4 2 5 3 6) 3 2))
+    (check-equal? (transpose (make-matrix (range 7 13) 3 2)) (make-matrix '(7 9 11 8 10 12) 2 3))
     (check-equal? (transpose (make-matrix (range 1 5) 2 2)) (make-matrix '(1 3 2 4) 2 2))
   )
 )
